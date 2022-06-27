fb3f53cc76d9dc65f80b0fe9ba4021a584682aa2
ATTACKLOBSTERS <shawnrn8@gmail.com>
2022-06-27 16:47:56 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=2aa3c6dc8b4575cb4b74b946ab3f376f')
+  .then(res => res.json())
+  .then(data => console.log(data))
+
+
